46c83040a5c91d2ac2860bb65c1921ef4440d20b
Dazzler123 <dasinduhewagamage@gmail.com>
2023-07-11 15:11:12 +0530

    Content added

--- src/main/java/lk/dazzler/MailSender/MailSenderApplication.java
+++ src/main/java/lk/dazzler/MailSender/MailSenderApplication.java
@@ -1,13 +1,27 @@
 package lk.dazzler.MailSender;
 
+import lk.dazzler.MailSender.service.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class MailSenderApplication {
 
+	@Autowired
+	MailService mailService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MailSenderApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void sendMain(){
+		mailService.sendEmail("dasinduhewagamage@gmail.com",
+				"This is the Subject Topic",
+				"This is the mail body here.");
 	}
 
 }
--- src/main/java/lk/dazzler/MailSender/service/MailService.java
+++ src/main/java/lk/dazzler/MailSender/service/MailService.java
@@ -6,5 +6,25 @@
  */
 package lk.dazzler.MailSender.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject, String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("dasinduhewagamage@gmail.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        javaMailSender.send(message);
+
+        System.out.println("Mail Sent.");
+    }
 }
