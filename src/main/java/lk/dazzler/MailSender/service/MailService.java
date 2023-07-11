730829de7155809074ca74f102500fac6f1572d0
Dazzler123 <dasinduhewagamage@gmail.com>
2023-07-11 15:08:38 +0530

    Introduced MailService

---
+++ src/main/java/lk/dazzler/MailSender/service/MailService.java
@@ -0,0 +1,10 @@
+/**
+ * #=== Created by Dazzler7Soft.Eng.Udg ===#
+ * Date : 11-Jul-2023
+ * Time : 3:08 PM
+ * Project Name : Mail-Sender
+ */
+package lk.dazzler.MailSender.service;
+
+public class MailService {
+}
